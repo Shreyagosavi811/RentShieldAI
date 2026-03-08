49ea51087bc0718df91b0852ff220580309fcb5b
Aditya Chavan <chavanaditya9325@gmail.com>
2026-03-08 18:22:06 +0530

    Added routes file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import RentShieldDashboard from './components/Rentshielddashboard'
+
+import CustomeRoutes from './routes/CustomeRoutes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <RentShieldDashboard/>
+    <div>
+      <CustomeRoutes/>
+    </div>
     </>
   )
 }
